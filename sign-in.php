ad6552abba2147ad521f787562a27a30375d72cf
Nhan Bui <buithithanhnhan18122005@gmail.com>
2026-01-14 11:19:26 +0700

    Cap nhat giao dien thanh tieng viet

--- sign-in.php
+++ sign-in.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Sign in | Grocery Mart</title>
+        <title>Đăng nhập | Grocery Mart</title>
 
         <!-- Favicon -->
         <link rel="apple-touch-icon" sizes="76x76" href="./assets/favicon/apple-touch-icon.png" />
@@ -28,8 +28,9 @@
             <div class="auth__intro d-md-none">
                 <img src="./assets/img/auth/intro.svg" alt="" class="auth__intro-img" />
                 <p class="auth__intro-text">
-                    The best of luxury brand values, high quality products, and innovative services
+                    Trải nghiệm những giá trị thương hiệu xa xỉ, sản phẩm chất lượng cao và dịch vụ sáng tạo nhất_
                 </p>
+                <p>Hãy đăng nhập để có trải nghiệm tốt nhất</p>
             </div>
 
             <!-- Auth content -->
@@ -37,20 +38,19 @@
                 <div class="auth__content-inner">
                     <a href="./" class="logo">
                         <img src="./assets/icons/logo.svg" alt="grocerymart" class="logo__img" />
-                        <h2 class="logo__title">grocerymart</h2>
+                        <h2 class="logo__title">Grocerymart</h2>
                     </a>
-                    <h1 class="auth__heading">Hello Again!</h1>
+                    <h1 class="auth__heading">Chào mừng bạn quay trở lại!</h1>
                     <p class="auth__desc">
-                        Welcome back to sign in. As a returning customer, you have access to your previously saved all
-                        information.
+                        Vui lòng đăng nhập để tiếp tục. Là khách hàng cũ, bạn có thể truy cập vào tất cả thông tin đã lưu trước đó
                     </p>
                     <form action="./index-logined.php" class="form auth__form">
                         <div class="form__group">
                             <div class="form__text-input">
                                 <input
                                     type="email"
-                                    name=""
-                                    id=""
+                                    name=""  
+                                    id=""    
                                     placeholder="Email"
                                     class="form__input"
                                     autofocus
@@ -59,15 +59,15 @@
                                 <img src="./assets/icons/message.svg" alt="" class="form__input-icon" />
                                 <img src="./assets/icons/form-error.svg" alt="" class="form__input-icon-error" />
                             </div>
-                            <p class="form__error">Email is not in correct format</p>
+                            <p class="form__error">Email không đúng định dạng</p>
                         </div>
                         <div class="form__group">
                             <div class="form__text-input">
                                 <input
                                     type="password"
-                                    name=""
+                                    name="" 
                                     id=""
-                                    placeholder="Password"
+                                    placeholder="Mật khẩu"
                                     class="form__input"
                                     required
                                     minlength="6"
@@ -75,27 +75,27 @@
                                 <img src="./assets/icons/lock.svg" alt="" class="form__input-icon" />
                                 <img src="./assets/icons/form-error.svg" alt="" class="form__input-icon-error" />
                             </div>
-                            <p class="form__error">Password must be at least 6 characters</p>
+                            <p class="form__error">Mật khẩu phải có ít nhất 6 kí tự</p>
                         </div>
                         <div class="form__group form__group--inline">
                             <label class="form__checkbox">
                                 <input type="checkbox" name="" id="" class="form__checkbox-input d-none" />
-                                <span class="form__checkbox-label">Set as default card</span>
+                                <span class="form__checkbox-label">Ghi nhớ đăng nhập</span>
                             </label>
-                            <a href="./reset-password.php" class="auth__link form__pull-right">Forgot password?</a>
+                            <a href="./reset-password.php" class="auth__link form__pull-right">Quên mật khẩu</a>
                         </div>
                         <div class="form__group auth__btn-group">
-                            <button class="btn btn--primary auth__btn form__submit-btn">Sign In</button>
+                            <button class="btn btn--primary auth__btn form__submit-btn">Đăng nhập</button>
                             <button class="btn btn--outline auth__btn btn--no-margin">
                                 <img src="./assets/icons/google.svg" alt="" class="btn__icon icon" />
-                                Sign in with Google
+                                Đăng nhập với Google
                             </button>
                         </div>
                     </form>
 
                     <p class="auth__text">
-                        Don’t have an account yet?
-                        <a href="./sign-up.php" class="auth__link auth__text-link">Sign Up</a>
+                        Bạn chưa có tài khoản ?
+                        <a href="./sign-up.php" class="auth__link auth__text-link">Đăng ký ngay</a>
                     </p>
                 </div>
             </div>
