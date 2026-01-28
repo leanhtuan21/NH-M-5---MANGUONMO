e62dad89116fad156ed985a79f304cec600a0683
nguyentrung23 <nntrungtb2003@gmail.com>
2026-01-28 17:03:14 +0700

    bỏ hover cart

--- templates/header-logined.php
+++ templates/header-logined.php
@@ -6,7 +6,7 @@
         </button>
 
         <!-- Logo -->
-        <a href="./index.php" class="logo top-bar__logo">
+        <a href="./index-logined.php" class="logo top-bar__logo">
             <img src="./assets/icons/logo.svg" alt="grocerymart" class="logo__img top-bar__logo-img" />
             <h1 class="logo__title top-bar__logo-title">grocerymart</h1>
         </a>
@@ -4637,88 +4637,7 @@
     <span class="top-act__title" style="color: inherit;">Cart</span> 
 </a>
 
-                    <!-- Dropdown -->
-                    <div class="act-dropdown">
-                        <div class="act-dropdown__inner">
-                            <img src="./assets/icons/arrow-up.png" alt="" class="act-dropdown__arrow" />
-                            <div class="act-dropdown__top">
-                                <h2 class="act-dropdown__title">You have 3 item(s)</h2>
-                                <a href="./checkout.php" class="act-dropdown__view-all">See All</a>
-                            </div>
-                            <div class="row row-cols-3 gx-2 act-dropdown__list">
-                                <!-- Cart preview item 1 -->
-                                <div class="col">
-                                    <article class="cart-preview-item">
-                                        <div class="cart-preview-item__img-wrap">
-                                            <img
-                                                src="./assets/img/product/item-1.png"
-                                                alt=""
-                                                class="cart-preview-item__thumb"
-                                            />
-                                        </div>
-                                        <h3 class="cart-preview-item__title">Lavazza Coffee Blends</h3>
-                                        <p class="cart-preview-item__price">$329.00</p>
-                                    </article>
-                                </div>
-
-                                <!-- Cart preview item 2 -->
-                                <div class="col">
-                                    <article class="cart-preview-item">
-                                        <div class="cart-preview-item__img-wrap">
-                                            <img
-                                                src="./assets/img/product/item-2.png"
-                                                alt=""
-                                                class="cart-preview-item__thumb"
-                                            />
-                                        </div>
-                                        <h3 class="cart-preview-item__title">Coffee Beans Espresso</h3>
-                                        <p class="cart-preview-item__price">$39.99</p>
-                                    </article>
-                                </div>
-
-                                <!-- Cart preview item 3 -->
-                                <div class="col">
-                                    <article class="cart-preview-item">
-                                        <div class="cart-preview-item__img-wrap">
-                                            <img
-                                                src="./assets/img/product/item-3.png"
-                                                alt=""
-                                                class="cart-preview-item__thumb"
-                                            />
-                                        </div>
-                                        <h3 class="cart-preview-item__title">Qualità Oro Mountain</h3>
-                                        <p class="cart-preview-item__price">$47.00</p>
-                                    </article>
-                                </div>
-                            </div>
-                            <div class="act-dropdown__bottom">
-                                <div class="act-dropdown__row">
-                                    <span class="act-dropdown__label">Subtotal</span>
-                                    <span class="act-dropdown__value">$415.99</span>
-                                </div>
-                                <div class="act-dropdown__row">
-                                    <span class="act-dropdown__label">Texes</span>
-                                    <span class="act-dropdown__value">Free</span>
-                                </div>
-                                <div class="act-dropdown__row">
-                                    <span class="act-dropdown__label">Shipping</span>
-                                    <span class="act-dropdown__value">$10.00</span>
-                                </div>
-                                <div class="act-dropdown__row act-dropdown__row--bold">
-                                    <span class="act-dropdown__label">Total Price</span>
-                                    <span class="act-dropdown__value">$425.99</span>
-                                </div>
-                            </div>
-                            <div class="act-dropdown__checkout">
-                                <a
-                                    href="./checkout.php"
-                                    class="btn btn--primary btn--rounded act-dropdown__checkout-btn"
-                                >
-                                    Check Out All
-                                </a>
-                            </div>
-                        </div>
-                    </div>
+                    
                 </div>
             </div>
 
